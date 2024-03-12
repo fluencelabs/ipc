b1068e37bfee63ae7fd169c9d461c2d2bb8dafce
Mike Voronov <michail.vms@gmail.com>
2024-03-12 04:14:46 +0300

    add batch support

--- fendermint/actors/fluence/src/types.rs
+++ fendermint/actors/fluence/src/types.rs
@@ -15,3 +15,14 @@ pub struct RandomXResult {
     #[serde(with = "strict_bytes")]
     pub result: [u8; TARGET_HASH_SIZE],
 }
+
+#[derive(Debug, Serialize_tuple, Deserialize_tuple)]
+pub struct RandomXArgumentsBatched {
+    pub global_nonce: Vec<Vec<u8>>,
+    pub local_nonce: Vec<Vec<u8>>,
+}
+
+#[derive(Debug, Serialize_tuple, Deserialize_tuple)]
+pub struct RandomXResultBatched {
+    pub result: Vec<[u8; TARGET_HASH_SIZE]>,
+}
